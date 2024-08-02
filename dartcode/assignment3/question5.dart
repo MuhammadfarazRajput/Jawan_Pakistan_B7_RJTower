a01a28788105c7d11e955227a9c6a694501bd0e1
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-02 22:42:29 +0500

    answer 5

---
+++ dartcode/assignment3/question5.dart
@@ -0,0 +1,14 @@
+void main(){
+
+   print("----------------------Question 5 -----------------------\n");
+ print("Write a program create two integer variables 'a' and 'b' and assign them any number and then check if both the conditions 'a < 50' and 'a < b' are true. Display the result, now check if at-least one of the conditions 'a < 50' and 'a < b' is true.\n"); 
+  
+  int a = 70;
+  int b = 100;
+  
+  print ("Result 1 =");
+  print(a<50&&a<b);
+  print ("Result 2 =");
+   print(a<50||a<b);
+
+}
