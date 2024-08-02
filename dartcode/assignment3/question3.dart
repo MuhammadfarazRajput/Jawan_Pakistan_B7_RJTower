9c18766c8cb4fa3885d0d21f9a6b03b045f9d9a7
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-02 22:39:59 +0500

    answer 3

---
+++ dartcode/assignment3/question3.dart
@@ -0,0 +1,13 @@
+void main(){
+    print("----------------------Question 3 -----------------------");
+
+print("Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area of the rectangle.Formula (area = length * breadth) \n");
+
+    double recLength = 5;
+    double recBreadth = 7;
+  
+  double recArea = recLength * recBreadth;
+  print ("Area of Rectangle is =  $recArea \n");
+ 
+
+}
