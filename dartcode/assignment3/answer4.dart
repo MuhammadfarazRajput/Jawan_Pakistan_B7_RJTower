1726f10f320f60c3fc8c3b462e28a13a0988e93b
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-02 22:41:01 +0500

    answer 4

---
+++ dartcode/assignment3/answer4.dart
@@ -0,0 +1,14 @@
+void main(){
+
+      print("----------------------Question 4 -----------------------");
+
+print("Create a integer variable num = 7, Add 8 to the number and then divide it by 3. Now, the modulus of that number is taken with 5 and then multiply the resultant value by 5, store result in variable i. Display the final result \n");
+  
+  num num1=7;
+  num num2 = (num1+8/3);
+  print (" $num1");
+  print (num2); 
+  num i = ((num2+5)*5);
+  print("$i \n");
+
+}
