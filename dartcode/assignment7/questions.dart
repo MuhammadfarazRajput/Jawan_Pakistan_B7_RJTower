38d381402668e22e67c93a3eb271025ec173615d
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-02 22:31:47 +0500

    update

--- dartcode/assignment7/questions.dart
+++ dartcode/assignment7/questions.dart
@@ -1,8 +1,11 @@
 void main(){
 
 Map std1 = {"name":"faraz","class":"12","sub1":"eng","sub2":"urdu","sub3":"math","sub4":"science",};
-//print(std1); 
-std1.firstOrNull;
 
-
+print(std1.isEmpty);
+print(std1.isNotEmpty);
+std1.clear();
+std1.remove(1);
+std1.length;
+print(std1); 
 }
