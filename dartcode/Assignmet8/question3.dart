6563b8cd1210c94116e75bc7994fa250fb40d098
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-05 21:29:41 +0500

    question 3

---
+++ dartcode/Assignmet8/question3.dart
@@ -0,0 +1,12 @@
+void main(){
+Map<String,num> myCountry ={'monday':234,'tuesday':23234,'wednesday':44,'thursday':8634,'friday':99234,'saturday':00234,'sunday':21134,};
+//print(myCountry);
+//print(myCountry.keys);
+//print(myCountry.values);
+
+//print(myCountry.keys.toList());
+//print(myCountry.values.toList());
+//print(myCountry.containsKey("sunday"));
+print(myCountry.remove("monday"));
+print(myCountry);
+}
