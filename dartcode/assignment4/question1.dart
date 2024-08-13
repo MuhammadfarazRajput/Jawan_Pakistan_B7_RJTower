b29ab836fef9f348d142429c52fd5d73f0a84335
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-13 12:29:17 +0500

    question 1

---
+++ dartcode/assignment4/question1.dart
@@ -0,0 +1,15 @@
+void main (){
+
+  print (" Write a Dart program to check if a given number is positive, negative, or zero.");
+
+  int num =0;
+  if (num > 0){
+print("given number is possitive");
+  }else if(num <0){
+    print("given number is positive");
+    }
+    else if (num == 0) {
+      print("given number is zero");
+    }
+  }
+  
