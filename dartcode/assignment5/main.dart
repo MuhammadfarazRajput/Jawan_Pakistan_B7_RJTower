55077666c182cdc60b4ba9fafb79958e4235eb47
muhammadfarazrajput <muhammadfaraz3241@gmail.com>
2024-08-13 12:06:17 +0500

    assignment 5 done

---
+++ dartcode/assignment5/main.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  print("Assignment 5 ");
+   print("Question 1\n Q.1) Create a list of names and print all names using list.");
+
+   List num_list = ["faraz","hassan","saqib"]; 
+   print(num_list); 
+
+ print("Question 2\n Q. 2) Create a list of Days and print only  Sunday");
+
+   List<String> num_list2 = ["monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]; 
+   print(num_list2[6]); 
+
+print("Question 3\n Q. 3)  create a list  of name, class, roll no, grade, percentage. And print. ");
+
+List dataEntry = ["hassan",2,0021,"A","50%","Saqib",3,0051,"B","90%"];
+ print(dataEntry);
+}
